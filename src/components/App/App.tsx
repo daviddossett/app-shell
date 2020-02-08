74e2352231734b3fbb0908e8a0cd7089f291603e
David Dossett <ddossett@microsoft.com>
2020-02-08 12:05:29 -0800

    Updated router

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,16 +3,16 @@ import { Page } from '../Page/Page';
 import { initializeIcons } from 'office-ui-fabric-react/lib/Icons';
 import { loadTheme } from 'office-ui-fabric-react/lib/Styling';
 import { palette } from '../../constants/theme';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 initializeIcons();
 loadTheme({palette});
 
 const App: React.FC = () => {
   return (
-    <Router basename ="/app-shell">>
+    <HashRouter basename="/">
       <Page />
-    </Router>
+    </HashRouter>
   );
 }
 
