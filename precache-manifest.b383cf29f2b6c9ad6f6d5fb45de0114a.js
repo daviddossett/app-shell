7f05be29cc357bfd7781bbbd77ca9bb02d66ef81
David Dossett <ddossett@microsoft.com>
2020-02-10 14:12:43 -0800

    Updates

---
+++ precache-manifest.b383cf29f2b6c9ad6f6d5fb45de0114a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d438ebcfaa004fdeaf67187629f05e74",
+    "url": "/app-shell/index.html"
+  },
+  {
+    "revision": "1b3164b3e524d01f0dba",
+    "url": "/app-shell/static/css/main.c4343487.chunk.css"
+  },
+  {
+    "revision": "85170485e2e94205c658",
+    "url": "/app-shell/static/js/2.38bb60af.chunk.js"
+  },
+  {
+    "revision": "339170d5fe0f99d1fa26e2ca9b049c1a",
+    "url": "/app-shell/static/js/2.38bb60af.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1b3164b3e524d01f0dba",
+    "url": "/app-shell/static/js/main.5252f9b7.chunk.js"
+  },
+  {
+    "revision": "7114eb20acad247d1fb8",
+    "url": "/app-shell/static/js/runtime-main.495715a0.js"
+  },
+  {
+    "revision": "738f8757ee276f65324013e29d0252c3",
+    "url": "/app-shell/static/media/product-icon.738f8757.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-shell/precache-manifest.ea31c764a2b24bdaf867787946bd6d6e.js"
+  "/app-shell/precache-manifest.b383cf29f2b6c9ad6f6d5fb45de0114a.js"
 );
 
 self.addEventListener('message', (event) => {
