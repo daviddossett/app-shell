91beee00379dc6c5d84437591448543a6377dea0
David Dossett <ddossett@microsoft.com>
2020-02-10 21:48:27 -0800

    Updates

--- precache-manifest.412b1b07900def069155ed65bcea2267.js
+++ precache-manifest.412b1b07900def069155ed65bcea2267.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "50746dae2a23578cdb724412fab74798",
+    "revision": "cd24419ccf90bb77a3461606ffb312ce",
     "url": "/app-shell/index.html"
   },
   {
-    "revision": "70b9559d816444c2fc4c",
+    "revision": "5d3433491857a15fd686",
     "url": "/app-shell/static/css/main.c4343487.chunk.css"
   },
   {
-    "revision": "99c27c705dc689283b82",
-    "url": "/app-shell/static/js/2.394563b9.chunk.js"
+    "revision": "5c4946d3f0ee5afbc831",
+    "url": "/app-shell/static/js/2.f28d8700.chunk.js"
   },
   {
     "revision": "339170d5fe0f99d1fa26e2ca9b049c1a",
-    "url": "/app-shell/static/js/2.394563b9.chunk.js.LICENSE"
+    "url": "/app-shell/static/js/2.f28d8700.chunk.js.LICENSE"
   },
   {
-    "revision": "70b9559d816444c2fc4c",
-    "url": "/app-shell/static/js/main.046482e8.chunk.js"
+    "revision": "5d3433491857a15fd686",
+    "url": "/app-shell/static/js/main.0e1ed416.chunk.js"
   },
   {
     "revision": "7114eb20acad247d1fb8",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/app-shell/precache-manifest.93e09eeaa25390d6c85f6c7c5f05f944.js"
+  "/app-shell/precache-manifest.412b1b07900def069155ed65bcea2267.js"
 );
 
 self.addEventListener('message', (event) => {
