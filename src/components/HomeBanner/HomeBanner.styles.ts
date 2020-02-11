2047c90a9fa375b7b47435c82608d6b8362ba06c
David Dossett <ddossett@microsoft.com>
2020-02-10 19:11:54 -0800

    Fixed grid layout

--- src/components/HomeBanner/HomeBanner.styles.ts
+++ src/components/HomeBanner/HomeBanner.styles.ts
@@ -1,4 +1,4 @@
-import { classNamesFunction, getTheme, FontSizes, FontWeights } from 'office-ui-fabric-react';
+import { classNamesFunction, getTheme, FontSizes, FontWeights, AnimationStyles } from 'office-ui-fabric-react';
 import { IHomeBannerStyleProps, IHomeBannerStyles } from './HomeBanner.types';
 import { breakpoint } from '../../constants/breakpoints';
 import { homePageBanner } from '../../constants/theme';
@@ -24,12 +24,12 @@ export const getStyles = (props: IHomeBannerStyleProps): IHomeBannerStyles => {
     root: {
       display: 'grid',
       gridTemplateColumns: '1fr 1fr 1fr',
-      gridTemplateRows: 'auto 100px 100px',
-      color: theme.palette.white,
+      gridTemplateRows: 'auto 100px auto',
+      color: theme.palette.white
     },
     banner: {
       gridColumn: '1 / span 3',
-      gridRow: '1 / span 3',
+      gridRow: '1 / span 2',
       background: homePageBanner,
     },
     wrapper: {
@@ -52,8 +52,7 @@ export const getStyles = (props: IHomeBannerStyleProps): IHomeBannerStyles => {
     },
     actions: {
       display: 'flex',
-      alignItems: 'center',
-      // marginBottom: '32px'
+      alignItems: 'center'
     },
     dismissButton: {
       alignSelf: 'center',
@@ -67,26 +66,23 @@ export const getStyles = (props: IHomeBannerStyleProps): IHomeBannerStyles => {
       alignSelf: 'center'
     },
     cardWrapper: {
-      padding: '16px',
+      padding: '0 16px 96px 16px',
       gridColumn: '1 / span 3',
-      gridRow: '2 / span 1'
+      gridRow: '2 / span 2'
     },
     card: {
       padding: '16px',
-      boxShadow: '0 1.6px 3.6px 0 rgba(0,0,0,.132), 0 0.3px 0.9px 0 rgba(0,0,0,.108)',
+      boxShadow: '0 3.2px 7.2px 0 rgba(0,0,0,.132), 0 0.6px 1.8px 0 rgba(0,0,0,.108)',
       borderRadius: '4px',
       backgroundColor: theme.palette.white,
       height: '400px',
-      gridColumn: '1 / span 3',
-      gridRow: '2 / span 1'
-
+      gridRow: '2 / span 2'
     },
     contentArea: {
       display: 'flex',
       alignItems: 'center',
       justifyContent: 'center',
       backgroundColor: 'rgba(252, 37, 118, 0.12)',
-      width: '100%',
       height: '100%'
     },
     text: {
