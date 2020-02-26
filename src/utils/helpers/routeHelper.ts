6b407d4c7ec921ac2d294593f75b374eeea99813
David Dossett <ddossett@microsoft.com>
2020-02-25 16:11:12 -0800

    Added route helper

---
+++ src/utils/helpers/routeHelper.ts
@@ -0,0 +1,33 @@
+import { Path } from 'history';
+
+enum DocumentTypes {
+  Project = 'project',
+  Report = 'report',
+  Signal = 'signal',
+  Source = 'source',
+  Segment = 'segment',
+  Metric = 'metric'
+}
+
+export function getProjectBasedResourcePath(
+  resourceType: DocumentTypes, 
+  projectId: string,
+  resourceId: string
+  ): Path {
+    const basePath = `/projects/${projectId}`;
+
+  switch (resourceType) {
+    case DocumentTypes.Report:
+      return `${basePath}/reports/${resourceId}`;
+    case DocumentTypes.Signal:
+      return `${basePath}/signals/${resourceId}`;
+    case DocumentTypes.Source:
+      return `${basePath}/sources/${resourceId}`;
+    case DocumentTypes.Segment:
+      return `${basePath}/segments/${resourceId}`;
+    case DocumentTypes.Metric:
+      return `${basePath}/metrics/${resourceId}`;
+    default:
+      throw new Error(`Unknown resource type ${resourceType}`);
+  }
+}
